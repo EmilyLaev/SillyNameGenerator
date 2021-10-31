da9a34c68ccaaa505f06836ef2189d776a62bca1
EmilyLaev <69812673+EmilyLaev@users.noreply.github.com>
2021-10-31 16:35:51 -0400

    Add files via upload

---
+++ Silly Name Generator.py
@@ -0,0 +1,21 @@
+import sys, random
+
+print("Welcome to the Silly Name Generator\n")
+print("Create a silly name:\n\n")
+first = ('Baby', 'Bad News', 'Burpy', 'Bill', 'Beenie', 'Bpb Stinkbug', 'Bowel', 'Boxelder', "Bud 'Lite'", 'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield', 'Chewy', 'Chigger', 'Worms')
+last = ('Apple', 'Bigmeat', 'Bloominshine', 'Booger-eater', 'Breeds', 'Clovenhoof', 'Clutterbuck')
+
+while True:
+    firstName = random.choice(first)
+    lastName = random.choice(last)
+
+    print("\n")
+    print("{} {}".format(firstName, lastName), file=sys.stderr)
+    print("\n")
+
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+    if try_again.lower() == "n":
+        break
+
+input("\nPress Enter to exit.")
+                
